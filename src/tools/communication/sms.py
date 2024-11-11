7aedca3fda8652ee86bab6f509ea07a34b022b8f
Joey <jyudel@gmail.com>
2024-11-11 13:10:07 -0500

    SMS integration

---
+++ src/tools/communication/sms.py
@@ -0,0 +1,21 @@
+import os
+import dotenv
+from twilio.rest import Client
+
+dotenv.load_dotenv()
+
+client = Client(
+    os.environ.get("TWILIO_ACCOUNT_SID"), 
+    os.environ.get("TWILIO_AUTH_TOKEN")
+)
+
+outgoing = os.environ.get("TWILIO_FROM_NUMBER")
+user_number = os.environ.get("USER_PHONE_NUMBER")
+
+def send_text(body: str):
+    client.messages.create(
+        from_=outgoing,
+        body=body,
+        to=user_number
+    )
+    
