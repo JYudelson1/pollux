c067fa552c6f975ae326de48b9aac0f4e299e213
Joey <jyudel@gmail.com>
2024-11-11 17:15:14 -0500

    Root and raw text

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import pathlib
+from typing import *
+
+ROOT = pathlib.Path(__file__).parent.parent
+
+def get_raw_text(messages: List[Dict]) -> str:
+    text = ""
+    
+    for message in messages: 
+        text += message["content"]["text"]
+        text += "\n"
+    
+    return text[:-1]
