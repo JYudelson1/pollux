9e862bd6b21fc0011efb8d2a2a40a2692d866b55
Joey <jyudel@gmail.com>
2024-11-11 19:45:18 -0500

    Memories can become XML

---
+++ src/tools/memory/xml.py
@@ -0,0 +1,21 @@
+from memory import Memory
+from datetime import datetime
+from typing import *
+from uuid import UUID
+
+def make_xml_once(memory: Memory, relevance: float) -> str:
+    return f'<memory id="{memory.uuid}" date="{timestamp(memory.timestamp)} relevance={relevance:.3f}" importance="{memory.importance}">\n\t{memory.content}\n</memory>'
+
+def make_xml(sorted_memories: List[Memory], relevances: Dict[UUID, float]) -> str:
+    text = '<system type="memory_load">'
+    
+    for memory in sorted_memories:
+        rel = relevances[memory.uuid]
+        text += make_xml_once(memory=memory, relevance=rel)
+    
+    text += '</system>'
+    
+    return text
+
+def timestamp(dt: datetime) -> str:
+    f"{dt.month} {dt.day} {dt.year} ({dt.hour}:{dt.minute})"
