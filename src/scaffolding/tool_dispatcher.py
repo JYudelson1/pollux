e19c19a6bd0e04b0ca06248e79493bc918abfcd9
Joey <jyudel@gmail.com>
2024-11-11 20:06:18 -0500

    More complete tool dispatching

--- src/scaffolding/tool_dispatcher.py
+++ src/scaffolding/tool_dispatcher.py
@@ -1,3 +1,5 @@
+from typing import *
+
 from src.tools.memory import MemoryTool
 from src.scaffolding.xml_parser import ParsedTag
 
@@ -5,19 +7,32 @@ class ToolServer:
     def __init__(self) -> None:
         self.memory_tool: MemoryTool = MemoryTool()
         
-    def use_tool(self, tag: ParsedTag) -> str:
+    def use_tools(self, tools: List[ParsedTag]) -> Optional[str]:
+        system_response = ""
+        
+        for tool in tools:
+            out = self.use_tool(tool)
+            if out is not None:
+                system_response += out
+                system_response += "\n"
+                
+        if system_response != "":
+            return system_response
+        
+    def use_tool(self, tag: ParsedTag) -> Optional[str]:
         try:
             output = self._try_use_tool(tag)
-            return f'<system type="{tag.tag}" status="success">{output}</system>'
+            if output is not None:
+                return f'<system type="{tag.tag}" status="success">{output}</system>'
         except Exception as e:
             return f'<system type="{tag.tag}" status="error">{e}</system>'
         
-    def _try_use_tool(self, tag: ParsedTag) -> str:
+    def _try_use_tool(self, tag: ParsedTag) -> Optional[str]:
         if tag.tag == "memory_load":
-            self.memory_tool.load_memories(query=tag.content, **tag.attributes)
+            return self.memory_tool.load_memories(query=tag.content, **tag.attributes)
         elif tag.tag == "memory_save":
-            self.memory_tool.load_memories(text=tag.content, **tag.attributes)
+            return self.memory_tool.save_memory(text=tag.content, **tag.attributes)
         elif tag.tag == "memory_delete":
-            self.memory_tool.delete_memory(uuid=tag.attributes["id"])
+            return self.memory_tool.delete_memory(uuid=tag.attributes["id"])
         else:
             raise NotImplementedError
